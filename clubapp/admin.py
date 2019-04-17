9b0de9f4757d2c0652892cfff09cf0c7f08383ec
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-04-16 22:21:24 -0700

    file changes for models and superuser

--- clubapp/admin.py
+++ clubapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Meeting, Minutes, Resource, Event
 
 # Register your models here.
+admin.site.register(Meeting)
+admin.site.register(Minutes)
+admin.site.register(Resource)
+admin.site.register(Event)
